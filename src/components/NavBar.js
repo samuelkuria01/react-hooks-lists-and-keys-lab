ff1859cb1891b382afe776ad323f669e55e5ed57
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-01-14 01:10:36 +0300

    Practise using .map() and creating list Create links to the home, about and project on the navBar. Create an object list of projects and use .map()to iterate through the list and use .key to get individual data.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+      <a href="#home" key={home}>home</a>
+      <a href="#about" key={about}>projects</a>
+      <a href="#projects" key={projects}>projects</a>
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,44 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
+
+const projects =[ 
+  {
+    id:1,
+    name:"Recipeplease",
+    about:"A recipe tracking app",
+    technologies:["Rails","Bootstrap CSS"],
+  },
+     {
+    id:2,
+    name:"Kibbles N Bitz",
+    about:"Tinder for dogs",
+    technologies:["React Redux"],
+  },
+{
+    id:3,
+    name:"Alienwares",
+    about:"Etsy for aliens",
+    technologies:["React Redux","Rails"],
+  },
+];
+
 function ProjectList({ projects }) {
-  console.log(projects);
-  return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
-    </div>
-  );
+
+  {Object.keys(projects).map((value, index)=>{
+
+    return (
+      <div id="projects" key={index}>
+        <h2>My Projects</h2>
+        <div id="project-list">{/* render ProjectItem components here */
+        <h3>{key}:{projects[key]}</h3>
+        }  
+        </div>
+      </div>
+    );
+
+  })};
+
 }
 
 export default ProjectList;
